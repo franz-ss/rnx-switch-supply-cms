542083f6841da4335def5620141f692a57e76f88
Franz Evangelista <franz@switchsupply.com>
2026-04-16 09:41:26 +0800

    chore: initialise Sanity Studio project

---
+++ sanity.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
+
+export default defineConfig({
+  name: "switch-supply",
+  title: "Switch Supply",
+  projectId: "kknhy9ol",
+  dataset: "production",
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title("Content")
+          .items([
+            S.listItem()
+              .title("Site Settings")
+              .id("siteSettings")
+              .child(
+                S.document()
+                  .schemaType("siteSettings")
+                  .documentId("siteSettings")
+              ),
+            S.divider(),
+            S.documentTypeListItem("ingredientCategory").title(
+              "Ingredient Categories"
+            ),
+            S.documentTypeListItem("testimonial").title("Testimonials"),
+          ]),
+    }),
+    visionTool(),
+  ],
+  schema: { types: schemaTypes },
+});
---
+++ schemaTypes/index.ts
@@ -0,0 +1,4 @@
+import { testimonial } from "./testimonial";
+import { ingredientCategory } from "./ingredientCategory";
+import { siteSettings } from "./siteSettings";
+export const schemaTypes = [testimonial, ingredientCategory, siteSettings];
